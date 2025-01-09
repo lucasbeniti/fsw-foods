8b0ee038e8c4fbc2703eab55588128ac8569b42f
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2025-01-09 20:51:53 -0300

    feat: add selected product page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,48 @@
+import { db } from "@/app/_lib/prisma";
+import { notFound } from "next/navigation";
+import ProductImage from "./_components/product-image";
+import ProductDetails from "./_components/product-details";
+
+interface ProductsPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const ProductsPage = async ({ params: { id } }: ProductsPageProps) => {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  const complementaryProducts = await db.product.findMany({
+    where: {
+      category: {
+        name: "Sucos",
+      },
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <ProductImage product={product} />
+      <ProductDetails
+        product={product}
+        complementaryProducts={complementaryProducts}
+      />
+    </div>
+  );
+};
+
+export default ProductsPage;
