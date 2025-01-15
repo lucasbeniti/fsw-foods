7200498696108bd5d99c453c1564c51c39b8eb4c
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2025-01-15 19:15:17 -0300

    feat: add recommended products page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,22 +1,30 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex justify-between px-5 pt-6">
-      <div className="relative h-[30px] w-[100px]">
-        <Image src="/logo.png" alt="FSW Foods" fill className="object-cover" />
-      </div>
+    <Link href="/">
+      <div className="flex justify-between px-5 pt-6">
+        <div className="relative h-[30px] w-[100px]">
+          <Image
+            src="/logo.png"
+            alt="FSW Foods"
+            fill
+            className="object-cover"
+          />
+        </div>
 
-      <Button
-        variant={"outline"}
-        size={"icon"}
-        className="border-none bg-transparent"
-      >
-        <MenuIcon />
-      </Button>
-    </div>
+        <Button
+          variant={"outline"}
+          size={"icon"}
+          className="border-none bg-transparent"
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+    </Link>
   );
 };
 
